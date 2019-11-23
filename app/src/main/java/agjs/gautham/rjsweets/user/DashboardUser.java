901e70c22b1640b9152fdd7fb73777f3835f31aa
GauthamAsir <gauthamasir@gmail.com>
2019-11-23 16:35:00 +0530

    User:: Dashboard: Dont show Login option unnecessary & Fix App Name typo
    Signed-off-by: GauthamAsir <gauthamasir@gmail.com>

--- app/src/main/java/agjs/gautham/rjsweets/user/DashboardUser.java
+++ app/src/main/java/agjs/gautham/rjsweets/user/DashboardUser.java
@@ -1,16 +1,28 @@
 package agjs.gautham.rjsweets.user;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.os.Handler;
 import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.view.GravityCompat;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -18,19 +30,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.view.View;
 import android.view.Window;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Map;
+
+import agjs.gautham.rjsweets.Common;
 import agjs.gautham.rjsweets.login.Login;
 import agjs.gautham.rjsweets.R;
 import agjs.gautham.rjsweets.user.navigation_drawer.cart_user.Cart;
 import agjs.gautham.rjsweets.user.navigation_drawer.home_user.Home;
 import agjs.gautham.rjsweets.user.navigation_drawer.order_user.Orders;
 import agjs.gautham.rjsweets.user.navigation_drawer.settings_user.Settings;
+import dmax.dialog.SpotsDialog;
+import io.paperdb.Paper;
 
 public class DashboardUser extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private long back_pressed;
+    FirebaseFirestore firestore;
+    FirebaseUser mUser;
 
     NavigationView navigationView;
     Menu menu;
@@ -65,6 +86,64 @@ public class DashboardUser extends AppCompatActivity implements NavigationView.O
 
         menu = navigationView.getMenu();
 
+        if (Common.isConnectedToInternet(getBaseContext())) {
+            Init();
+        }else {
+            menu.findItem(R.id.nav_login).setVisible(false);
+            menu.findItem(R.id.nav_logout).setVisible(false);
+            Toast.makeText(DashboardUser.this,"No Internet Connection !",Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    public void Init(){
+
+        if (Common.isConnectedToInternet(getBaseContext())) {
+
+            View headerView = navigationView.getHeaderView(0);
+            final TextView t1 = headerView.findViewById(R.id.header_txt);
+
+            Typeface typeface = Typeface.createFromAsset(getAssets(),"fonts/NABILA.TTF");
+            t1.setTypeface(typeface);
+
+            firestore = FirebaseFirestore.getInstance();
+
+            FirebaseAuth mAuth = FirebaseAuth.getInstance();
+            mUser = mAuth.getCurrentUser();
+
+            if (mUser != null) {
+                String email = mUser.getEmail();
+
+                assert email != null;
+                DocumentReference documentReference = firestore.collection("users").document(email);
+                documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot documentSnapshot = task.getResult();
+                            assert documentSnapshot != null;
+                            if (documentSnapshot.exists()) {
+                                Map<String, Object> userInfo = documentSnapshot.getData();
+                                assert userInfo != null;
+                                String uName = (String) userInfo.get("Username");
+                                String uPhone = (String) userInfo.get("Number");
+                                t1.setText("Hello, " + uName);
+                                Common.USER_Phone = uPhone;
+                                Common.Name = uName;
+                            }
+                        }
+                    }
+                });
+
+                menu.findItem(R.id.nav_login).setVisible(false);
+            } else {
+                menu.findItem(R.id.nav_logout).setVisible(false);
+            }
+
+        }else {
+            Toast.makeText(DashboardUser.this,"No Internet Connection !",Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
@@ -99,7 +178,52 @@ public class DashboardUser extends AppCompatActivity implements NavigationView.O
 
             case R.id.nav_logout:
 
-                toast("Log-Out");
+                final FirebaseAuth mAuth;
+                mAuth = FirebaseAuth.getInstance();
+
+                final android.app.AlertDialog dialog;
+
+                dialog = new SpotsDialog.Builder()
+                        .setContext(this)
+                        .setCancelable(false)
+                        .setMessage("Logging You Out !...")
+                        .setTheme(R.style.DialogCustom)
+                        .build();
+
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Log Out !");
+                builder.setMessage("Do you really want to Log Out ?");
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        dialog.show();
+                        new Handler().postDelayed(new Runnable() {
+                            public void run() {
+                                if (dialog.isShowing()){
+                                    dialog.dismiss();
+                                }
+                                mAuth.signOut();
+
+                                Paper.book().destroy();
+                                Intent logout = new Intent(DashboardUser.this,Login.class);
+                                logout.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(logout);
+                                finish();
+                                overridePendingTransition(R.anim.activity_close_enter, R.anim.activity_close_exit );
+                            }
+                        }, 3000);
+                    }
+                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //Return To Resumed Fragments
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+                alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(getApplicationContext().getColor(R.color.colorText));
+                alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(getApplicationContext().getColor(R.color.colorText));
                 break;
 
             case R.id.nav_login:
@@ -132,9 +256,5 @@ public class DashboardUser extends AppCompatActivity implements NavigationView.O
                 back_pressed = System.currentTimeMillis();
             }
         }
-    }
-
-    private void toast(String msg){
-        Toast.makeText(DashboardUser.this,msg,Toast.LENGTH_LONG).show();
     }
 }
